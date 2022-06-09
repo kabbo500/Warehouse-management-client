0e36f6e9f22f48cffb64620224c4f177e488f0ab
kabbo500 <kabbokazi3779@gmail.com>
2022-06-09 19:24:41 +0800

    add

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('products.json')
+        fetch('https://gentle-gorge-41754.herokuapp.com/service')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, []);
